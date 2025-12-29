5f3d8a10ee87947d431b3d4b3c0fab2a151fcf93
jayshree gaikawad <jayshregaikawad@gmail.com>
2025-12-29 19:27:11 +0530

    Read for deployement

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.use("/api/v1/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("create the server");
+})
+
+
+const start = async () => {
+    const connectionDB = await mongoose.connect("mongodb+srv://jayshregaikawad_db_user:dUuA0Q3mVc9uWCEN@cluster0.hkzwsjz.mongodb.net/");
+    console.log(`MONGO Connected DB Host: ${connectionDB.connection.host}`);
+
+    server.listen(app.get("port"), "127.0.0.1", () => {
+        console.log("Listening on port 8000");
+    })
+}
+start();
